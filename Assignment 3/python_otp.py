0a8b9cdb64584b001b9034cd34c84d69f97d8c9d
suyashkolhe25 <82031106+suyashkolhe25@users.noreply.github.com>
2022-12-16 15:56:10 +0530

    Update python_otp.py

--- Assignment 3/python_otp.py
+++ Assignment 3/python_otp.py
@@ -1,7 +1,5 @@
 """
     This is a Object Oriented Program to send OTP to the given email id using python smtplib
-
-    Author: umairkarel
 """
 from dotenv import load_dotenv, find_dotenv
 from os import environ as env
